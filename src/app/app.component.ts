fc9636b5c3c39865f8b67cc517d0676fa43e733a
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-03-03 01:38:53 +0100

    feat(app): add vis.js timeline example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import * as vis from 'vis';
 
 @Component({
     selector: 'app',
@@ -6,6 +7,26 @@ import { Component, ViewEncapsulation } from '@angular/core';
     styleUrls: ['./app.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     model = 1;
+
+    ngAfterViewInit() {
+        var container = document.getElementById('visualization');
+
+        // Create a DataSet (allows two way data-binding)
+        var items = new vis.DataSet([
+            { id: 1, content: 'item 1', start: '2013-04-20' },
+            { id: 2, content: 'item 2', start: '2013-04-14' },
+            { id: 3, content: 'item 3', start: '2013-04-18' },
+            { id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19' },
+            { id: 5, content: 'item 5', start: '2013-04-25' },
+            { id: 6, content: 'item 6', start: '2013-04-27' }
+        ]);
+
+        // Configuration for the Timeline
+        var options = {};
+
+        // Create a Timeline
+        var timeline = new vis.Timeline(container, items, options);
+    }
 }
